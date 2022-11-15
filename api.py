a6ba2c208ba0a55ba9ebffd8eb6505117cc55275
arko24 <62396421+arko24@users.noreply.github.com>
2022-11-15 09:39:07 +0700

    Add files via upload

---
+++ api.py
@@ -0,0 +1,22 @@
+import subprocess
+import json
+import requests as rq
+import time, sys, datetime, os, click
+
+
+#script 
+subprocess.call("cls", shell=True)
+
+#class
+def xharga(coin):
+  url = 'https://api.binance.com/api/v3/ticker/24hr'
+  r = rq.get(url)
+  js = json.loads(r.text)
+  return js
+
+#parameter
+bd = xharga('')
+crn = ('BUSD')
+
+#tampil
+print(bd)
