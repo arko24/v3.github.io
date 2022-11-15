b60d4caf8043a677e16878c95673b1bb39cf24c2
arko24 <62396421+arko24@users.noreply.github.com>
2022-11-15 17:24:06 +0700

    Add files via upload

--- fapi.py
+++ fapi.py
@@ -9,19 +9,25 @@ subprocess.call("clear", shell=True)
 
 #class
 def xharga(coin):
-  url = 'https://fapi.binance.com/futures/data/globalLongShortAccountRatio'
+  url = 'https://fapi.binance.com/fapi/v1/ticker/24hr'
   r = rq.get(url)
   js = json.loads(r.text)
   return js
 
+def xharga2(coin):
+  url = 'https://fapi.binance.com/futures/data/topLongShortPositionRatio?symbol='
+  r = rq.get(url)
+  js = json.loads(r.text)
+  return js
 
 #parameter
 bd = xharga('')
+bd2 = xharga2('')
 crn = ('USDT')
-persen = ('-0.10')
+param = ('&period=5m&limit=1')
 
 #tampil
-#for i in range(len(bd)):
-#    print(f"{bd[i]['symbol']}" + f"{bd[i]['lastPrice']}")
-print(bd)
+for i in range(len(bd)):
+    c1 =(f"{bd[i]['symbol']}")
+    print(f"{bd2&(c1)&(param)})
 
