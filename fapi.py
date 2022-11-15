2b1811883293d5ecd805bb3194ad4ffed449554d
arko24 <62396421+arko24@users.noreply.github.com>
2022-11-15 21:51:47 +0700

    Update fapi.py

--- fapi.py
+++ fapi.py
@@ -14,9 +14,20 @@ def xharga(coin):
   js = json.loads(r.text)
   return js
 
+def xharga2(coin):
+  url = 'https://fapi.binance.com/futures/data/topLongShortAccountRatio?symbol=BTCUSDT&period=5m&limit=10'
+  r = rq.get(url)
+  js = json.loads(r.text)
+  return js
+
 #parameter
 bd = xharga('')
+bd2 = xharga2('')
 
 #tampil
 for i in range(len(bd)):
     print(f"{bd[i]['longAccount']}"+' | '+f"{bd[i]['shortAccount']}")
+#tampil
+for i in range(len(bd2)):
+    print(f"{bd2[i]['longAccount']}"+' | '+f"{bd2[i]['shortAccount']}")
+
