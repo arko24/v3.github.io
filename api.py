dc0fdffce714232d7c3b80b94dcb992586f9d891
arko24 <62396421+arko24@users.noreply.github.com>
2022-11-15 11:54:47 +0700

    Update api.py

--- api.py
+++ api.py
@@ -22,5 +22,5 @@ persen = ('0.05')
 #tampil
 for i in range(len(bd)):
   if (bd[i]['symbol'][-4:]) == (crn) and (bd[i]['priceChange']) < (persen):
-    print(f"{i}. {bd[i]['symbol']}" +'  |  '+ f"{i}. {bd[i]['lastPrice']}")
+    print(f"{bd[i]['symbol']}" +'  |  '+ f"{bd[i]['lastPrice']}")
 
